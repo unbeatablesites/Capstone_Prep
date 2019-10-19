8d75400f94472aeea65b5529ed5b6ab365e6ef75
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-19 15:59:35 -0400

    Largest 5

--- CapStonePrep/src/main/java/ArrayFunUserArray.java
+++ CapStonePrep/src/main/java/ArrayFunUserArray.java
@@ -7,9 +7,39 @@ public class ArrayFunUserArray {
 
         Scanner scan = new Scanner(System.in);
 
-        String userInput = scan.nextLine();
+        System.out.println(" Please enter a number ");
 
-        int numbers = Integer.parseInt(userInput);
+        String userInput1 = scan.nextLine();
+
+        int number1 = Integer.parseInt(userInput1);
+
+        System.out.println(" Please enter a number ");
+
+        String userInput2 = scan.nextLine();
+
+        int number2 = Integer.parseInt(userInput2);
+
+        System.out.println(" Please enter a number ");
+
+        String userInput3 = scan.nextLine();
+
+        int number3 = Integer.parseInt(userInput3);
+
+        System.out.println(" Please enter a number ");
+
+        String userInput4 = scan.nextLine();
+
+        int number4 = Integer.parseInt(userInput4);
+
+        System.out.println(" Please enter a number ");
+
+        String userInput5 = scan.nextLine();
+
+        int number5 = Integer.parseInt(userInput5);
+
+
+
+
 
 
     }
