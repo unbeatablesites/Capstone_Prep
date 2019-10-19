532c4deda9635daf1c9e2623e6d7e630037d1f17
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-19 15:52:57 -0400

    Largest 5

---
+++ CapStonePrep/src/main/java/ArrayFunUserArray.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ArrayFunUserArray {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+
+        String userInput = scan.nextLine();
+
+        int numbers = Integer.parseInt(userInput);
+
+
+    }
+}
