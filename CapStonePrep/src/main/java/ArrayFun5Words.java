f67c6c722c4765c007a3c84a07741185eff46008
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-19 19:34:17 -0400

    Array Fun Words It

---
+++ CapStonePrep/src/main/java/ArrayFun5Words.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayFun5Words {
+
+    public static void main(String[] args) {
+
+        wordsIntoArray();
+
+    }
+
+
+    public static void wordsIntoArray(){
+
+
+        Scanner scan = new Scanner(System.in);
+        String[] array = new String[5];
+
+        int i = 0;
+
+        while (i < 5){
+
+            for(i = 0; i < 5; i++){
+                System.out.println("Enter a word to enter into the array");
+                array[i] = scan.nextLine();
+            }
+        }
+
+            System.out.println(Arrays.toString(array));
+
+    }
+}
+
