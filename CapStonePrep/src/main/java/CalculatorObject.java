5472b148009209ba380be5b78296e12c7bb3fb0d
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-20 13:44:00 -0400

    Calculator

---
+++ CapStonePrep/src/main/java/CalculatorObject.java
@@ -0,0 +1,30 @@
+public class CalculatorObject {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int sub(int a , int b){
+        return a-b;
+    }
+
+    public int multi(int a, int b){
+        return a*b;
+    }
+
+    public int div(int a , int b){
+        return a/b;
+    }
+
+    public double add(double a, double b){
+        return a+b;
+    }
+
+    public double sub(double a, double b){
+        return a-b;
+    }
+
+    public double multi(double a, double b){
+        return a/b;
+    }
+}
