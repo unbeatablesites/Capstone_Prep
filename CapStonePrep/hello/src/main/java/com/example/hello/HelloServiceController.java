5a01481d14c38a040cc299caac20bcb54272059e
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-27 19:14:40 -0400

    Made a hello service controller that returns a string

---
+++ CapStonePrep/hello/src/main/java/com/example/hello/HelloServiceController.java
@@ -0,0 +1,15 @@
+package com.example.hello;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloServiceController {
+
+    @RequestMapping(value = "/name/{name}",method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String whatsYourName(@PathVariable String name){
+        return "It's really nice to meet you " + name;
+    }
+
+}
