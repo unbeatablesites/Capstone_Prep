648dd14098a3757d863221e32f52d1fa7a6ad4c0
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-27 21:47:06 -0400

    Made weather Api Controller

--- CapStonePrep/hello/src/main/java/com/example/hello/model/WeatherServiceController.java
+++ CapStonePrep/hello/src/main/java/com/example/hello/model/WeatherServiceController.java
@@ -1,4 +1,29 @@
 package com.example.hello.model;
 
+import com.example.hello.Temprature;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@RestController
 public class WeatherServiceController {
+
+    @RequestMapping(value = "/temprature/{zipCode}",method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public Temprature getTemp(@PathVariable String zipCode) {
+        if (zipCode.length() != 5) {
+            throw new IllegalArgumentException("Zip code must be 5 characters long");
+        }
+
+
+        Random rnd = new Random();
+        Temprature temp = new Temprature();
+
+        temp.setFahrenheit(rnd.nextInt(140) - 20);
+        double celsiusTemp = (temp.getFahrenheit() - 32) *  5/9;
+        temp.setCelsius((int) celsiusTemp);
+        return temp;
+    }
+
 }
