76aca1d838e04e18fbe87230d51659c7e193f7c8
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-27 20:06:23 -0400

    Made calculator Api

---
+++ CapStonePrep/hello/src/main/java/com/example/hello/CalculatorObject.java
@@ -0,0 +1,24 @@
+package com.example.hello;
+
+public class CalculatorObject {
+
+
+    private int operand1;
+    private int operand2;
+
+    public int getOperand1() {
+        return operand1;
+    }
+
+    public void setOperand1(int operand1) {
+        this.operand1 = operand1;
+    }
+
+    public int getOperand2() {
+        return operand2;
+    }
+
+    public void setOperand2(int operand2) {
+        this.operand2 = operand2;
+    }
+}
---
+++ CapStonePrep/hello/src/main/java/com/example/hello/calculatorController.java
@@ -0,0 +1,33 @@
+package com.example.hello;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class calculatorController {
+
+    @RequestMapping(value = "/add",method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public int add(@RequestBody CalculatorObject calculatorObject){
+    return calculatorObject.getOperand1() + calculatorObject.getOperand2();
+    }
+
+    @RequestMapping(value = "/subtract",method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public int sub(@RequestBody CalculatorObject calculatorObject){
+        return calculatorObject.getOperand1() - calculatorObject.getOperand2();
+    }
+
+    @RequestMapping(value = "/multi",method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public int multipy(@RequestBody CalculatorObject calculatorObject){
+        return calculatorObject.getOperand1() *calculatorObject.getOperand2();
+    }
+
+    @RequestMapping(value = "/divide",method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public float divide(@RequestBody CalculatorObject calculatorObject){
+        return calculatorObject.getOperand1() / calculatorObject.getOperand2();
+    }
+
+}
