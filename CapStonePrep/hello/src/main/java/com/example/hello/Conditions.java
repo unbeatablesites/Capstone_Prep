69f74614e27370699f880818d21f3326e4f1befe
Frank Uzoka <Frankuzoka@hotmail.com>
2019-10-27 21:14:10 -0400

    Made weather Api objects

---
+++ CapStonePrep/hello/src/main/java/com/example/hello/Conditions.java
@@ -0,0 +1,60 @@
+package com.example.hello;
+
+public class Conditions {
+
+    private int fahrenheit;
+    private int celsius;
+    private int windSpeed;
+    private String windDirection;
+    private String skies;
+    private String precipitation;
+
+    public int getFahrenheit() {
+        return fahrenheit;
+    }
+
+    public void setFahrenheit(int fahrenheit) {
+        this.fahrenheit = fahrenheit;
+    }
+
+    public int getCelsius() {
+        return celsius;
+    }
+
+    public void setCelsius(int celsius) {
+        this.celsius = celsius;
+    }
+
+    public int getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(int windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getWindDirection() {
+        return windDirection;
+    }
+
+    public void setWindDirection(String windDirection) {
+        this.windDirection = windDirection;
+    }
+
+    public String getSkies() {
+        return skies;
+    }
+
+    public void setSkies(String skies) {
+        this.skies = skies;
+    }
+
+    public String getPrecipitation() {
+        return precipitation;
+    }
+
+    public void setPrecipitation(String precipitation) {
+        this.precipitation = precipitation;
+    }
+
+}
---
+++ CapStonePrep/hello/src/main/java/com/example/hello/Temprature.java
@@ -0,0 +1,23 @@
+package com.example.hello;
+
+public class Temprature {
+
+    private int fahrenheit;
+    private int celsius;
+
+    public int getFahrenheit() {
+        return fahrenheit;
+    }
+
+    public void setFahrenheit(int fahrenheit) {
+        this.fahrenheit = fahrenheit;
+    }
+
+    public int getCelsius() {
+        return celsius;
+    }
+
+    public void setCelsius(int celsius) {
+        this.celsius = celsius;
+    }
+}
